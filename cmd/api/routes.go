79c380e91c24c1fbece4a5a9bec5124559bd4412
ica1130 <ilijakrilovic@gmail.com>
2024-05-09 13:09:43 +0200

    feat: createMovieHandler, showMovieHandler.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,8 +10,8 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthckeckHandler)
-	//router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	//router.HandlerFunc(http.MethodGet, "/v1/movies", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 	return router
 }
