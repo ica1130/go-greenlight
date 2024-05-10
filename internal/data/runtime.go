90374669499df9931b7a625a4ac0c3d372de9ed5
ica1130 <ilijakrilovic@gmail.com>
2024-05-10 16:44:21 +0200

    feat: movies.go can unmarshal runtime custom struct.

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -19,4 +23,29 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSON, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSON, " ")
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
