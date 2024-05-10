8fa5f3321a0db362bac25c39f5f4936f6f423f4e
ica1130 <ilijakrilovic@gmail.com>
2024-05-10 16:28:55 +0200

    refactor: readJSON will check for max bytes.

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -77,6 +77,8 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		case strings.HasPrefix(err.Error(), "json: unknown field "):
 			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
 			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
 		default:
 			return err
 		}
