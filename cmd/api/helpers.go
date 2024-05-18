4a23006919dce6a1309a7f7be8aaa204f9253656
ica1130 <ilijakrilovic@gmail.com>
2024-05-18 16:00:06 +0200

    feat: readString helper.

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
@@ -90,4 +91,12 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 	}
 
 	return nil
+}
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	return s
 }
