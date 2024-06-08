f26d89780f54d32dedf8e3e41ad59eb55879cf15
ica1130 <ilijakrilovic@gmail.com>
2024-06-08 16:35:46 +0200

    refactor: main.go includes cors config.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"mygreenlight/internal/jsonlog"
 	"mygreenlight/internal/mailer"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -69,6 +73,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "f8ad96a7dd2b5f", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "75aa7c7e17a868", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.ilijakrilovic.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
