4a365d39bc50710e6dcaec5483a50bcd5ea49fdd
ica1130 <ilijakrilovic@gmail.com>
2024-06-10 20:22:35 +0200

    feat: created metrics middleware to count totalRequestsReceived, totalResponsesSent, totalProcessingTime.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"mygreenlight/internal/data"
 	"mygreenlight/internal/validator"
@@ -207,5 +208,24 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) metrics(next http.Handler) http.Handler {
+	totalRequestReceived := expvar.NewInt("total_requests_received")
+	totalResponsesSent := expvar.NewInt("total_responses_sent")
+	totalProcessingTimeMicroseconds := expvar.NewInt("total_procesing_time_ms")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		totalRequestReceived.Add(1)
+
+		next.ServeHTTP(w, r)
+
+		totalResponsesSent.Add(1)
+
+		duration := time.Now().Sub(start).Microseconds()
+		totalProcessingTimeMicroseconds.Add(duration)
 	})
 }
