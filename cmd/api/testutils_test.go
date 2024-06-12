ad937186611d3b046411176db18acee65a12e29d
ica1130 <ilijakrilovic@gmail.com>
2024-06-12 14:40:42 +0200

    feat: created get request helper.

--- cmd/api/testutils_test.go
+++ cmd/api/testutils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -21,4 +22,19 @@ func newTestApplication(t *testing.T) *application {
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
 	return &testServer{ts}
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
+	res, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return res.StatusCode, res.Header, body
 }
