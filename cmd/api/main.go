a27f3bfd62d3a8d72019d1f6d428fbed1e085445
ica1130 <ilijakrilovic@gmail.com>
2024-06-16 13:42:30 +0200

    feat: user can check the build version.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"mygreenlight/internal/data"
 	"mygreenlight/internal/jsonlog"
 	"mygreenlight/internal/mailer"
@@ -81,7 +82,14 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("version:\t%s\n", version)
+		os.Exit(0)
+	}
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
